eaa3cef7750ba85f14562849acc99cf5906715f2
anushatalluri4 <67678515+anushatalluri4@users.noreply.github.com>
2026-05-26 19:16:19 +0530

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        self.ind = 0
+        self.maxlen = 0
+        for i in range(len(s)):
+            self.pal(s,i,i)
+            self.pal(s,i,i+1)
+        return s[self.ind:self.ind+self.maxlen]
+    def pal(self,s,l,r):
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            if r-l+1>self.maxlen:
+                self.maxlen = r-l+1
+                self.ind = l
+            l-=1
+            r+=1
